b96b59fbe9b7a9b101c5ed3b17204363e05f347f
SethDe1 <78003608+SethDe1@users.noreply.github.com>
2023-04-21 18:38:58 -0500

    Creating UI

--- Example.py
+++ Example.py
@@ -1,6 +1,7 @@
 import socket
 import threading
 import random
+import tkinter 
 
 # Define the game rules
 rules = {
---
+++ Game.py
@@ -0,0 +1,60 @@
+from tkinter import *
+
+#Creating the main window
+mainPanel = Tk()
+mainPanel.title("Rock Paper Scissors")
+mainPanel.geometry("275x125")
+var = IntVar()
+
+#setting the choice
+def sel():
+    if var.get() == 1:
+        choice = "rock"
+    elif var.get() == 2:
+        choice = "paper"
+    elif var.get() == 3:
+        choice = "scissors"
+
+    #Displaying choice on screen
+    selection = "You choose " + choice
+    label.config(text = selection)
+
+#Send choice to server
+def buttonPush():
+    #send choice to server
+    top= Toplevel(mainPanel)
+    top.geometry("275x125")
+    top.title("Results")
+
+    playerResults = 1
+
+    if (playerResults == 1):
+        result = "You won!"
+    else:
+        result = "You lost!"
+    Label(top, text= result).pack()
+
+
+
+#Rock button
+R1 = Radiobutton(mainPanel, text="Rock", variable=var, value=1, command=sel)
+R1.pack( anchor = W )
+
+#Paper button
+R2 = Radiobutton(mainPanel, text="Paper", variable=var, value=2, command=sel)
+R2.pack( anchor = W )
+
+#Scissors button
+R3 = Radiobutton(mainPanel, text="Scissors", variable=var, value=3, command=sel)
+R3.pack( anchor = W)
+
+#Submit button
+B = Button(text ="Submit", command = buttonPush)
+
+
+
+#packing panels to the main window
+label = Label(mainPanel)
+label.pack()
+B.pack()
+mainPanel.mainloop()
